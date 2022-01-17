985daf3f5a2a7f23a1f3c95d92e61767a34937ca
Gabriel Dias <g.dias1@hotmail.com>
2022-01-16 21:11:28 -0300

    Get method

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,4 +4,7 @@ const productController = require("../controllers/product.controller");
 //Create a new product
 router.post("/products", productController.createProduct);
 
+//List all of products
+router.get("/products", productController.listAllProducts);
+
 module.exports = router;
